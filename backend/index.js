08bfbf9ff962c745c2b3e9e1c6f9fba8657e26c8
Naimul Islam <ms0413354@gmail.com>
2023-05-13 21:22:37 +0600

    connect mongoose and run successfully server

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// ADD MADILWARE
+app.use(cors());
+app.use(express.json());
+
+// ADD MONGOOSE
+mongoose
+	.connect(
+		`mongodb+srv://multi-vendor:${process.env.DB_PASS}@cluster0.x3ohfcq.mongodb.net/?retryWrites=true&w=majority`
+	)
+	.then(() => console.log('Multi-Vendor Database Connected Successfully!'))
+	.catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+	res.send('<h1>How are you?</h1>');
+});
+
+app.use('*', (req, res) => {
+	const { baseUrl } = req;
+	res.send(`<h1>${baseUrl} Not Found!</h1>`);
+});
+
+app.listen(port, () => {
+	console.log(`Multi-Vendor Server Running on Port: ${port}`);
+});
