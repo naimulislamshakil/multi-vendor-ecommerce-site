b3f7cb9e134e83eb86373a575712f7c50c2b8df2
Naimul Islam <ms0413354@gmail.com>
2023-05-18 22:58:40 +0600

    Create a user from postman

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+const UserRoute = require('./Routes/v1/User.route');
 require('dotenv').config();
 
 // ADD MADILWARE
@@ -11,11 +12,11 @@ app.use(express.json());
 
 // ADD MONGOOSE
 mongoose
-	.connect(
-		`mongodb+srv://multi-vendor:${process.env.DB_PASS}@cluster0.x3ohfcq.mongodb.net/?retryWrites=true&w=majority`
-	)
+	.connect('mongodb://localhost:27017')
 	.then(() => console.log('Multi-Vendor Database Connected Successfully!'))
 	.catch((err) => console.log(err));
+
+app.use('/api/v1', UserRoute);
 
 app.get('/', (req, res) => {
 	res.send('<h1>How are you?</h1>');
